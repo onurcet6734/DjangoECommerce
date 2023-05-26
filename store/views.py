8c74c51b25c17e76221cdf44f2328db0acc501a8
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2023-05-26 18:34:20 +0300

    dropdown has been included on the project

--- store/views.py
+++ store/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import *
+from .models import Category, Product
 
 def index(request):
     products = Product.objects.all()
-    context = {'products':products}
-    return render(request,"index.html",context)
-
+    categories = Category.objects.all()
+    context = {'products': products, 'categories': categories}
+    return render(request, "index.html", context)
