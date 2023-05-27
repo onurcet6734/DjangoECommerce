5afb6bb887feb8ba5fe27ceb629b83def3f6fb15
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2023-05-27 13:45:54 +0300

    jquery and filter have been included on the project

--- store/views.py
+++ store/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 def index(request):
     products = Product.objects.all()
     categories = Category.objects.all()
-    context = {'products': products, 'categories': categories}
+
+    # Kategori filtresi uygula
+    category_id = request.GET.get('category_id')
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    context = {
+        'products': products,
+        'categories': categories
+    }
     return render(request, "index.html", context)
 
 def detail_view(request):
