a6c612da5688b2528e958ee887a2eb9776290217
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2023-06-18 13:44:03 +0300

    Relations of the Address class have been set

--- store/models.py
+++ store/models.py
@@ -8,13 +8,14 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Customer(models.Model):
-	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-	name = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200)
 
-	def __str__(self):
-		return self.name
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -25,12 +26,28 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     image = models.ImageField(null=True, blank=True)
 
-
     def __str__(self):
         return self.title
 
 
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Order #{self.pk}"
+
+
 class Address(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
     address_line1 = models.CharField(max_length=100)
     address_line2 = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=100)
@@ -41,23 +58,10 @@ class Address(models.Model):
         return f"{self.address_line1}, {self.city}, {self.state}"
 
 
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    def save(self, *args, **kwargs):
-        self.total_price = self.product.price * self.quantity
-        super().save(*args, **kwargs)
-    
-
-
 class Payment(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
-    payment_method = models.CharField(max_length=100, blank=False)  
-    transaction_id = models.CharField(max_length=100, blank=False)  
+    payment_method = models.CharField(max_length=100, blank=False)
+    transaction_id = models.CharField(max_length=100, blank=False)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
 
