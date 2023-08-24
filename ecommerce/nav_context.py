4a0a7803bb649e09ae4f0591c3b493afd871a310
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2023-08-24 18:24:11 +0300

    the routine maintenances

--- ecommerce/nav_context.py
+++ ecommerce/nav_context.py
@@ -1,9 +1,11 @@
 from store.models import Category
 from store.api.serializers import CategorySerializer
+from store.utils import get_customer_from_cookie
 
 def nav_data(request):
      category_serializer = CategorySerializer(Category.objects.all(), many=True)
-     context = {'categories': category_serializer.data,}
+     customer_from_cookie = get_customer_from_cookie(request)
+     context = {'categories': category_serializer.data,'customer_from_cookie': customer_from_cookie,}
      return context
       
             
