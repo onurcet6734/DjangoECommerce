12a74dbb6ecb12efdfbe359554436bf547d63a15
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2026-03-14 17:36:36 +0300

    revizing on view layer

--- store/urls.py
+++ store/urls.py
@@ -6,11 +6,10 @@ urlpatterns = [
     path('detail/<int:product_id>/', views.ProductDetailView.as_view(), name='detail'),
     path('cart/', views.CartView.as_view(), name='cart'),
     path('add-to-cart/<int:product_id>/', views.AddToCartView.as_view(), name='add_to_cart'),
-    path('delete_order/<int:order_id>/', views.delete_order, name='delete_order'),
-    path('add_comment/<int:product_id>', views.add_comment, name='add_comment'),
+    path('delete_order/<int:order_id>/', views.DeleteOrderView.as_view(), name='delete_order'),
+    path('add_comment/<int:product_id>/', views.AddCommentView.as_view(), name='add_comment'),
     path('address-form/', views.CheckoutView.as_view(), name='checkout'),
     path('accounts/login/', views.HandledLoginView.as_view(), name='handledLogin'),
-    path('payment_checkout/', views.payment_checkout, name='payment_checkout'),
-    path('success/', views.success_view, name='success_view'),
-
+    path('payment_checkout/', views.PaymentCheckoutView.as_view(), name='payment_checkout'),
+    path('success/', views.SuccessView.as_view(), name='success_view'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Category, Product, Order, Customer, Address,Comment
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import JsonResponse
 from django.db.models import Sum, Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-from .utils import set_customer_cookie, get_customer_from_cookie, delete_customer_cookie
-from store.api.serializers import ProductSerializer, OrderSerializer, CustomerSerializer, CategorySerializer
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.conf import settings
 import stripe
-from rest_framework.views import APIView
-from django.utils.decorators import method_decorator
-from store.models import CommentForm
-from django.views import View
 
-class IndexView(APIView):
+from rest_framework.views import APIView
+
+from .models import Product, Order, Customer, Address, Comment
+from .utils import set_customer_cookie, get_customer_from_cookie
+from store.api.serializers import ProductSerializer, OrderSerializer, CustomerSerializer
+from store.models import CommentForm
+
+class IndexView(View):
+
+
     def get(self, request):
+
         products = Product.objects.select_related('category').all()
 
         category_id = request.GET.get('category_id')
@@ -23,306 +28,364 @@ class IndexView(APIView):
 
         search_query = request.GET.get('q')
         if search_query:
-            products = products.filter(Q(title__icontains=search_query) | Q(description__icontains=search_query))
+            products = products.filter(
+                Q(title__icontains=search_query) |
+                Q(description__icontains=search_query)
+            )
 
         total_item_count = 0
-        customer_from_cookie = get_customer_from_cookie(request)
-        
-        custnumberid = Customer.objects.filter(name = customer_from_cookie).values_list('id', flat=True)
-        adminOrders = Order.objects.filter(is_admin_product = True)
-        if customer_from_cookie!=None:
-            adminOrders.update(is_admin_product=False, customer = custnumberid)
 
         if request.user.is_authenticated:
             try:
-                customer = Customer.objects.select_related('user').get(user=request.user)
+                customer = Customer.objects.get(user=request.user)
                 total_item_count = Order.objects.filter(customer=customer).count()
             except Customer.DoesNotExist:
                 pass
 
-        product_serializer = ProductSerializer(products, many=True, context={'request': request})
-
         context = {
-            'products': product_serializer.data,
+            'products': products,
             'total_item_count': total_item_count,
-            'search_query': search_query,
-            'customer_from_cookie': customer_from_cookie,
-
+            'search_query': search_query
         }
+
         return render(request, "index.html", context)
 
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
 
 class HandledLoginView(APIView):
+
+
+    def get(self, request):
+        return render(request, 'login.html')
+
     def post(self, request):
+
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
+
             login(request, user)
+
             try:
                 customer = Customer.objects.get(user=user)
-                customer.update_name_from_user()  
+                customer.update_name_from_user()
+
                 response = redirect('index')
                 set_customer_cookie(response, customer)
+
                 return response
+
             except Customer.DoesNotExist:
-                pass
-            return redirect('index')
-        else:
-            return render(request, 'login.html', {'message': 'Geçersiz kullanıcı adı veya şifre girişi yaptınız!'})
+                return redirect('index')
 
-    def get(self, request):
+        return render(
+            request,
+            'login.html',
+            {'message': 'Geçersiz kullanıcı adı veya şifre!'}
+        )
 
-        return render(request, 'login.html')
 
 class CartView(APIView):
+
+
     @method_decorator(login_required)
     def get(self, request):
-        orders = Order.objects.select_related('customer', 'product').filter(customer__user=request.user)
-        total_price_sum = orders.aggregate(Sum('total_price'))['total_price__sum']
+
+        orders = Order.objects.select_related(
+            'customer',
+            'product'
+        ).filter(customer__user=request.user)
+
+        total_price_sum = orders.aggregate(
+            Sum('total_price')
+        )['total_price__sum']
+
         total_item_count = orders.count()
+
         cargo_price = 20
 
         if total_price_sum and total_price_sum > 5000:
             cargo_price = 0
 
-        order_serializer = OrderSerializer(orders, many=True, context={'request': request})
+        order_serializer = OrderSerializer(
+            orders,
+            many=True,
+            context={'request': request}
+        )
 
         context = {
             'orders': order_serializer.data,
             'total_price_sum': total_price_sum,
             'total_item_count': total_item_count,
-            'cargo_price': cargo_price,
+            'cargo_price': cargo_price
         }
 
         return render(request, "cart.html", context)
 
+
 class AddToCartView(APIView):
+
+
     def post(self, request, product_id):
+
         product = get_object_or_404(Product, pk=product_id)
 
-        if request.method == 'POST':
-            quantity = request.POST.get('quantity', '1')
+        quantity = int(request.POST.get('quantity', 1))
 
-            if request.user.is_authenticated:
-                try:
-                    customer = Customer.objects.get(user=request.user)
-                    order = Order(
-                        customer=customer,
-                        product=product,
-                        quantity=quantity,
-                        total_price=product.price * int(quantity)
-                    )
-                    order.save()
+        if request.user.is_authenticated:
 
-                    product_serializer = ProductSerializer(product, context={'request': request})
-                    customer_serializer = CustomerSerializer(customer)
-
-                    return JsonResponse({
-                        'message': 'Item was added to cart',
-                        'product': product_serializer.data,
-                        'customer': customer_serializer.data
-                    })
-                except Customer.DoesNotExist:
-                    return JsonResponse({'message': 'Kullanıcı hiçbir müşteriye sahip değil'})
-            else:
-                return JsonResponse({'message': 'Yetkisiz kullanıcı!'})
-
-        return redirect('index')
-
-def delete_order(request,order_id):
-    order = get_object_or_404(Order, id=order_id)
-    order.delete()
-    return redirect('cart')
-
-
-def add_comment(request, product_id):
-    url = request.META.get("HTTP_REFERER") #bir önceki url i alır
-    customer = Customer.objects.get(user=request.user)
-    product = get_object_or_404(Product, id=product_id)
-
-    checkCompletedOrder = Order.objects.select_related('customer').get(customer=customer.id).is_completed
-    if checkCompletedOrder:
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = Comment(
-                customer=customer,
-                product=product,
-                comment=form.cleaned_data['comment'],
-                rate=form.cleaned_data['rate'],
-            )
-            comment.save()
-            return redirect(url)
-        
-    return redirect("index") 
-
-
-class ProductDetailView(View):
-    def get(self, request, product_id):
-        product = get_object_or_404(Product, id=product_id)
-        customer , created = Customer.objects.get_or_create(user=request.user)
-        total_item_count = Order.objects.filter(customer=customer).count()
-        comments = Comment.objects.select_related('customer', 'product').filter(product = product_id)
-        return render(request, 'detail.html', 
-                        {
-                          'comments':comments,
-                          'product': product, 
-                          'total_item_count': total_item_count
-                        }
-                    )
-
-    @method_decorator(login_required)
-    def post(self, request, product_id):
-        product = get_object_or_404(Product, id=product_id)
-        customer, created = Customer.objects.get_or_create(user=request.user)
-
-        if request.method == 'POST':
-            quantity = int(request.POST.get('quantity', 1))
             try:
-                order = Order.objects.select_related('customer', 'product').get(customer=customer, product=product)
-                order.quantity += quantity
-                order.total_price = product.price * order.quantity
-                order.save()
-            except Order.DoesNotExist:
-                order = Order(
+                customer = Customer.objects.get(user=request.user)
+
+                order = Order.objects.create(
                     customer=customer,
                     product=product,
                     quantity=quantity,
                     total_price=product.price * quantity
                 )
-                order.save()
-            return redirect('cart')
+
+                product_serializer = ProductSerializer(
+                    product,
+                    context={'request': request}
+                )
+
+                customer_serializer = CustomerSerializer(customer)
+
+                return JsonResponse({
+                    'message': 'Item added to cart',
+                    'product': product_serializer.data,
+                    'customer': customer_serializer.data
+                })
+
+            except Customer.DoesNotExist:
+                return JsonResponse({'message': 'Customer not found'})
+
+        return JsonResponse({'message': 'Unauthorized user'})
+
+
+class DeleteOrderView(View):
+
+    @method_decorator(login_required)
+    def post(self, request, order_id):
+
+        order = get_object_or_404(
+            Order,
+            id=order_id,
+            customer__user=request.user
+        )
+
+        order.delete()
+
+        return redirect('cart')
+
+
+class ProductDetailView(View):
+
+
+    def get(self, request, product_id):
+
+        product = get_object_or_404(Product, id=product_id)
+
+        customer, created = Customer.objects.get_or_create(user=request.user)
+
+        total_item_count = Order.objects.filter(customer=customer).count()
+
+        comments = Comment.objects.select_related(
+            'customer',
+            'product'
+        ).filter(product=product_id)
+
+        return render(
+            request,
+            'detail.html',
+            {
+                'comments': comments,
+                'product': product,
+                'total_item_count': total_item_count
+            }
+        )
+
+
+    @method_decorator(login_required)
+    def post(self, request, product_id):
+
+        product = get_object_or_404(Product, id=product_id)
+
+        customer, created = Customer.objects.get_or_create(user=request.user)
+
+        quantity = int(request.POST.get('quantity', 1))
+
+        try:
+
+            order = Order.objects.get(
+                customer=customer,
+                product=product
+            )
+
+            order.quantity += quantity
+            order.total_price = product.price * order.quantity
+            order.save()
+
+        except Order.DoesNotExist:
+
+            Order.objects.create(
+                customer=customer,
+                product=product,
+                quantity=quantity,
+                total_price=product.price * quantity
+            )
+
+        return redirect('cart')
+
+
+class AddCommentView(View):
+
+
+    @method_decorator(login_required)
+    def post(self, request, product_id):
+
+        url = request.META.get("HTTP_REFERER")
+
+        customer = Customer.objects.get(user=request.user)
+
+        product = get_object_or_404(Product, id=product_id)
+
+        check_completed = Order.objects.filter(
+            customer=customer,
+            is_completed=True
+        ).exists()
+
+        if check_completed:
+
+            form = CommentForm(request.POST)
+
+            if form.is_valid():
+
+                Comment.objects.create(
+                    customer=customer,
+                    product=product,
+                    comment=form.cleaned_data['comment'],
+                    rate=form.cleaned_data['rate']
+                )
+
+                return redirect(url)
+
+        return redirect("index")
+
 
 class CheckoutView(APIView):
+
+
     @method_decorator(login_required)
     def get(self, request):
+
         customer = get_object_or_404(Customer, user=request.user)
-        orders = Order.objects.filter(customer=customer).prefetch_related('product')
+
+        orders = Order.objects.filter(customer=customer)
 
         if not orders.exists():
             return redirect('index')
 
-        total_price_sum = orders.aggregate(Sum('total_price'))['total_price__sum']
+        total_price_sum = orders.aggregate(
+            Sum('total_price')
+        )['total_price__sum']
+
         total_item_count = orders.count()
 
-
         context = {
-            'total_item_count': total_item_count,
             'total_price_sum': total_price_sum,
+            'total_item_count': total_item_count
         }
 
-        if get_customer_from_cookie(request):
-            return render(request, 'address_form.html', context)
-        else:
-            return render(request, 'login.html')
+        return render(request, 'address_form.html', context)
+
+
+class PaymentCheckoutView(View):
+
 
     @method_decorator(login_required)
     def post(self, request):
+
         customer = get_object_or_404(Customer, user=request.user)
-        orders = Order.objects.filter(customer=customer).prefetch_related('product')
+
+        orders = Order.objects.filter(
+            customer=customer
+        ).prefetch_related("product")
+
         if not orders.exists():
-            return redirect('index')
-        total_price_sum = orders.aggregate(Sum('total_price'))['total_price__sum']
-        total_item_count = orders.count()
+            return redirect("cart")
 
-        context = {
-            'total_item_count': total_item_count,
-            'total_price_sum': total_price_sum,
+        latest_order = orders.latest("created_at")
 
-        }
-        if get_customer_from_cookie(request):
-            return render(request, 'address_form.html', context)
-        else:
-            return render(request, 'login.html')
+        Address.objects.create(
+            customer=customer,
+            order=latest_order,
+            address_line1=request.POST.get("address_line1"),
+            address_line2=request.POST.get("address_line2"),
+            city=request.POST.get("city"),
+            state=request.POST.get("state"),
+            postal_code=request.POST.get("postal_code")
+        )
 
-# stripe.api_key = settings.STRIPE_SECRET_KEY
-# checkout_session = stripe.checkout.Session.create(
-#         payment_method_types=[
-#             'card',
-#         ],
-#         line_items=[
-#             {
-#                 'price': 'price_1NRMgrDGClv24mi8EMjZuTe7',
-#                 'quantity': 1,
-#             },
-#         ],
-#         mode='payment',
-#         success_url='http://localhost:8000/success/',
-#         cancel_url='http://127.0.0.1:8000/fail',
-#     )
+        line_items = []
 
-# @login_required
-def payment_checkout(request):
+        for order in orders:
 
-    if request.method != "POST":
-        return redirect("checkout")
-
-    customer = get_object_or_404(Customer, user=request.user)
-    orders = Order.objects.filter(customer=customer).prefetch_related("product")
-
-    if not orders.exists():
-        return redirect("cart")
-
-    latest_order = orders.latest("created_at")
-
-    address_line1 = request.POST.get("address_line1")
-    address_line2 = request.POST.get("address_line2")
-    city = request.POST.get("city")
-    state = request.POST.get("state")
-    postal_code = request.POST.get("postal_code")
-
-    Address.objects.create(
-        customer=customer,
-        order=latest_order,
-        address_line1=address_line1,
-        address_line2=address_line2,
-        city=city,
-        state=state,
-        postal_code=postal_code
-    )
-
-    line_items = []
-
-    for order in orders:
-        line_items.append({
-            "price_data": {
-                "currency": "usd",
-                "product_data": {
-                    "name": order.product.title,
+            line_items.append({
+                "price_data": {
+                    "currency": "try",
+                    "product_data": {
+                        "name": order.product.title
+                    },
+                    "unit_amount": int(order.product.price * 100)
                 },
-                "unit_amount": int(order.product.price * 100),
-            },
-            "quantity": order.quantity,
-        })
+                "quantity": order.quantity
+            })
 
-    stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    checkout_session = stripe.checkout.Session.create(
-        payment_method_types=["card"],
-        line_items=line_items,
-        mode="payment",
-        success_url="http://localhost:8000/success/?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url="http://localhost:8000/fail/",
-    )
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=line_items,
+            mode="payment",
+            success_url="http://localhost:8000/success/?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url="http://localhost:8000/cart/"
+        )
 
-    return redirect(checkout_session.url, code=303)
-#STOKTAN DUSME 
-def success_view(request):
-    session_id = request.GET.get("session_id")
-    if not session_id:
+        return redirect(session.url, code=303)
+
+
+class SuccessView(View):
+
+
+    def get(self, request):
+
+        session_id = request.GET.get("session_id")
+
+        if not session_id:
+            return render(request, "success.html")
+
+        session = stripe.checkout.Session.retrieve(session_id)
+
+        if session.payment_status == "paid":
+
+            addresses = Address.objects.select_related(
+                "order",
+                "order__product"
+            )
+
+            for address in addresses:
+
+                product = address.order.product
+
+                product.stock -= address.order.quantity
+                product.save()
+
+                Order.objects.filter(
+                    product=product
+                ).update(is_completed=True)
+
         return render(request, "success.html")
-    session = stripe.checkout.Session.retrieve(session_id)
-    if session.payment_status == "paid":
-        addresses = Address.objects.select_related("order", "order__product")
-        for address in addresses:
-            product = address.order.product
-            product.stock = product.stock - address.order.quantity
-            product.save()
-            Order.objects.filter(product=product).update(is_completed=True)
-        return render(request, "success.html")
-    return render(request, "success.html")
-    
+
