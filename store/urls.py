ad3ed79eabfafc670c265741f82b73e85827a429
TechWorm <TechWorm@DESKTOP-U9GLI9A>
2026-03-27 17:06:47 +0300

    revizing on card structure

--- store/urls.py
+++ store/urls.py
@@ -1,14 +1,28 @@
 from django.urls import path
-from . import views
+from django.contrib.auth.views import LogoutView
+from .views import (
+    IndexView,
+    RegisterView,
+    HandledLoginView,
+    CartView,
+    DeleteOrderView,
+    ProductDetailView,
+    AddCommentView,
+    CheckoutView,
+    PaymentCheckoutView,
+    SuccessView,
+)
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('detail/<int:product_id>/', views.ProductDetailView.as_view(), name='detail'),
-    path('cart/', views.CartView.as_view(), name='cart'),
-    path('delete_order/<int:order_id>/', views.DeleteOrderView.as_view(), name='delete_order'),
-    path('add_comment/<int:product_id>/', views.AddCommentView.as_view(), name='add_comment'),
-    path('address-form/', views.CheckoutView.as_view(), name='checkout'),
-    path('accounts/login/', views.HandledLoginView.as_view(), name='handledLogin'),
-    path('payment_checkout/', views.PaymentCheckoutView.as_view(), name='payment_checkout'),
-    path('success/', views.SuccessView.as_view(), name='success_view'),
+    path("", IndexView.as_view(), name="index"),
+    path("logout/", LogoutView.as_view(next_page="index"), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("accounts/login/", HandledLoginView.as_view(), name="handledLogin"),
+    path("cart/", CartView.as_view(), name="cart"),
+    path("cart/delete/<int:order_id>/", DeleteOrderView.as_view(), name="delete_order"),
+    path("product/<int:product_id>/", ProductDetailView.as_view(), name="product_detail"),
+    path("product/<int:product_id>/comment/", AddCommentView.as_view(), name="add_comment"),
+    path("checkout/", CheckoutView.as_view(), name="checkout"),
+    path("checkout/payment/", PaymentCheckoutView.as_view(), name="payment_checkout"),
+    path("success/", SuccessView.as_view(), name="success"),
 ]
